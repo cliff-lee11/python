fa1c8bd8c7f9a5a94eb61769b57f3d9fc60329df
Cliff Lee <cliff_lee11@naver.com>
2019-01-17 17:58:13 +0900

    commit comfirm

--- studyPython/baekjoon/q2579_Climbing_stairs.py
+++ studyPython/baekjoon/q2579_Climbing_stairs.py
@@ -33,8 +33,7 @@
 '''
 import sys 
 
-def calMaxScore(stairs):
-    '''  마지막 계단은 밟아야 함 
+'''  마지막 계단은 밟아야 함 
     stair[N] 에 도착 하려면, 
         1. stair[N-1] -> stair[N]
                   =>  여기서  stair[N-2]는 밟으면 안된다. 즉 Stair[N-1]을 밟았다면, Stair[N-3]을 밟아야 한다. 
@@ -43,37 +42,51 @@ def calMaxScore(stairs):
                즉  어떤 계단 N까지 가려면,  stair[N-3] + stair[N-1] + Stair[N]
                              stair[N-2] + Stair[N]
     
-    '''
-    step = []
-    step.append(stairs[-1])
+          여기서 처음 3개는 임의로 구해서 넣고, 해당  계단을 계산할때 가장 작은 위치 의 값은 누적값을 이전 계단과 현재 계단값은 입력값을 넣는다. 
+          전부 누적값을 사용할 경우, Scores[n-1] 에는  Scores[N-3] 위치가 고려된 MAX 값이라  연속 체크가 어렵고, 같은값이 두번 연산 된다. 
+         하지만 모든 값을 입력 값을 사용하면, 이전 계산된  누적값을 잃어 버리고, 현재 계단 기준으로 값이 계산 되므로 오답이 된다.   
     
-    pos = len(stairs)-1
-    while pos >= 3:
-        tmp1 = stairs[pos-3] + stairs[pos-1]
-        tmp2 = stairs[pos-2]
+         그리고 N이 3 미만의 값이면, 수동으로 계산한 값을 처리해 준다. 만들어진 식이 N-3 이  0 보다 작으면 안되기 때문이다.  
+'''
+
+def calMaxScore(stairs,N):
+
+    if N == 0:
+        return 0
+    
+    else:
+        stepScores = [0]
         
-        if tmp1 > tmp2:
-            step.append(stairs[pos-1])
-            step.append(stairs[pos-3])
-            pos = pos -3
-            
+        if N < 3:
+            if N == 1:
+                return stairs[N]
+            else:
+                return stairs[N]+ stairs[N-1]
         else:
-            step.append(stairs[pos-2])
-            pos = pos -2
+            for i in range(1,3):
+                stepScores.append(stepScores[i-1]+ stairs[i])            
+    
+    for i in range(3,len(stairs)):
+        tmp1 = stepScores[i-3] + stairs[i-1] + stairs[i]
+        tmp2 = stepScores[i-2] + stairs[i]
+        
+        stepScores.append(max(tmp1,tmp2))
         
         
-    print(step)
-    return sum(step) 
+    #print(stepScores)
+    
+    return stepScores[N]
      
     
 N = int(input())
 
 stairs = [0]
 
+
 for _ in range(N):
     stairs.append(int(sys.stdin.readline()))
 
-print(calMaxScore(stairs))
+print(calMaxScore(stairs,N))
 
 
 
